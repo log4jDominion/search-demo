f9105422c51abdc2ecf85ed2efc7787b3aa84463
log4jDominion <bhardwajshashank1506@gmail.com>
2025-02-10 15:40:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -675,4 +675,4 @@ if __name__ == '__main__':
     init_db()
     pt.java.set_java_home("/home/ubuntu/search-demo/jdk-22.0.2/")
     # app.run(host="0.0.0.0", port=8889, debug=True)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
